151763ca75e612526e67d8346d5e6b9a797b5715
Moshe <moisherosenzweig@gmail.com>
2021-12-25 21:20:17 -0500

    Howzit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+
+
+
+    const main = document.getElementById("main")
+    main.remove()
+
+    const newHeader = document.createElement("h1")
+    document.body.appendChild(newHeader)
+newHeader.id = "victory"
+newHeader.textContent = "Moshe is the champion";
+
+
+
